0d8542e6cc1e97afafdbfb9f5a92954c42cd24c9
Stefan Caras <stefan.caras@gmail.com>
2023-12-02 20:44:00 +0200

    projects

---
+++ 6.py
@@ -0,0 +1,12 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_left()
+#         turn_left()
+#         turn_left()
+#         move()
+#     else:
+#         turn_left()
+#     while wall_on_right() and front_is_clear():
+#         move()
