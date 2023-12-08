de05bb7542f15515fdd565213a2b6c1046e2e2fe
Stefan Caras <stefan.caras@gmail.com>
2023-12-08 16:53:43 +0200

    new project

--- 6.py
+++ 6.py
@@ -1,6 +1,14 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
-# while not at_goal():
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# while True:
+#     while wall_on_right() and front_is_clear():
+#         move()
+#     if at_goal():
+#         break
 #     if right_is_clear():
 #         turn_left()
 #         turn_left()
@@ -8,5 +16,4 @@
 #         move()
 #     else:
 #         turn_left()
-#     while wall_on_right() and front_is_clear():
-#         move()
+    
